97f51dfd63155bcdfcd6114403ae5733a0d2a748
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-06 10:20:53 +0900

    instagram clone project init.

---
+++ instagramClone/instagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by Kwangjun Kim on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
